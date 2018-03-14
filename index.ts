85f460ff6db4a789a7d67b5bac92ff342210a368
Jason Stiles <neosyler@gmail.com>
2018-03-14 14:05:11 -0400

    Removing exported member

--- index.ts
+++ index.ts
@@ -3,14 +3,12 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 import {LSelect2Component} from './select2/select2.component';
-import {LSelect2Validator} from "./select2/select2.validator";
 
 export * from './select2/select2.component';
 
 @NgModule({
   declarations: [
-    LSelect2Component,
-    LSelect2Validator
+    LSelect2Component
   ],
   imports: [
     CommonModule,
